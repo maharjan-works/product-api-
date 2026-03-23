5b8d2f75baa1eafc82218b2342e7caa6bd30f2bf
maharjan.works <maharjan.works@gmail.com>
2026-03-23 19:47:58 -0400

    get all categories api created

--- src/main/java/com/maharjanworks/product_api/service/CategoryService.java
+++ src/main/java/com/maharjanworks/product_api/service/CategoryService.java
@@ -2,12 +2,15 @@ package com.maharjanworks.product_api.service;
 
 import com.maharjanworks.product_api.dto.CategoryDTO;
 
+import java.util.List;
+
 public interface CategoryService {
 
     //create Category
     CategoryDTO createCategory(CategoryDTO dto);
 
     //get all categories
+    List<CategoryDTO> getCategories();
 
     //get category by id
 
