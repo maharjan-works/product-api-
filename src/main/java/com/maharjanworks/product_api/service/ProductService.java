6e51979aa89c5cb24efd0782f8a4b18c40fafed4
maharjan.works <maharjan.works@gmail.com>
2026-03-23 21:17:59 -0400

    mysql setup and createProduct api created

--- src/main/java/com/maharjanworks/product_api/service/ProductService.java
+++ src/main/java/com/maharjanworks/product_api/service/ProductService.java
@@ -1,4 +1,18 @@
 package com.maharjanworks.product_api.service;
 
+import com.maharjanworks.product_api.dto.ProductDTO;
+
 public interface ProductService {
+
+    //create product
+    ProductDTO createProduct(ProductDTO productDTO);
+
+    //get all products
+
+    //get product by id
+
+    //update product
+
+    //delete product
+
 }
